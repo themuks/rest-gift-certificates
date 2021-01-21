9577a974fd8c7689aa672f0d272c7974e31a8648
Maksim Kuntsevich <max-cun4@yandex.ru>
2021-01-21 18:07:44 +0300

    refactor: datasource parameters initialization via annotations

--- app/src/main/java/com/epam/esm/config/DatabaseConfig.java
+++ app/src/main/java/com/epam/esm/config/DatabaseConfig.java
@@ -1,31 +1,30 @@
 package com.epam.esm.config;
 
 import org.apache.commons.dbcp.BasicDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 import javax.sql.DataSource;
-import java.util.ResourceBundle;
 
 @Configuration
+@PropertySource("classpath:database.properties")
 public class DatabaseConfig {
-    private static final String DATABASE_BUNDLE_NAME = "database";
-    private static final String DB_URL = "db.url";
-    private static final String DB_USER = "db.user";
-    private static final String DB_PASSWORD = "db.password";
-    private static final String DB_DRIVER = "db.driver";
-    private static final String DB_POOL_SIZE = "db.poolSize";
+    @Value("${db.driver}")
+    private String driver;
+    @Value("${db.user}")
+    private String user;
+    @Value("${db.password}")
+    private String password;
+    @Value("${db.url}")
+    private String url;
+    @Value("${db.poolSize}")
+    private int poolSize;
 
     @Bean
     public DataSource mySqlDataSource() {
         BasicDataSource dataSource = new BasicDataSource();
-        ResourceBundle resourceBundle = ResourceBundle.getBundle(DATABASE_BUNDLE_NAME);
-        String url = resourceBundle.getString(DB_URL);
-        String user = resourceBundle.getString(DB_USER);
-        String password = resourceBundle.getString(DB_PASSWORD);
-        String driver = resourceBundle.getString(DB_DRIVER);
-        String poolSizeStr = resourceBundle.getString(DB_POOL_SIZE);
-        int poolSize = Integer.parseInt(poolSizeStr);
         dataSource.setDriverClassName(driver);
         dataSource.setUsername(user);
         dataSource.setPassword(password);
--- app/src/test/java/com/epam/esm/config/DatabaseTestConfig.java
+++ app/src/test/java/com/epam/esm/config/DatabaseTestConfig.java
@@ -1,24 +1,24 @@
 package com.epam.esm.config;
 
 import org.apache.commons.dbcp.BasicDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 import javax.sql.DataSource;
-import java.util.ResourceBundle;
 
 @Configuration
+@PropertySource("classpath:database.properties")
 public class DatabaseTestConfig {
-    private static final String DATABASE_BUNDLE_NAME = "database";
-    private static final String DB_URL = "db.url";
-    private static final String DB_DRIVER = "db.driver";
+    @Value("${db.driver}")
+    private String driver;
+    @Value("${db.url}")
+    private String url;
 
     @Bean
     public DataSource h2DataSource() {
         BasicDataSource dataSource = new BasicDataSource();
-        ResourceBundle resourceBundle = ResourceBundle.getBundle(DATABASE_BUNDLE_NAME);
-        String url = resourceBundle.getString(DB_URL);
-        String driver = resourceBundle.getString(DB_DRIVER);
         dataSource.setDriverClassName(driver);
         dataSource.setUrl(url);
         return dataSource;
